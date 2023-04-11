210e626dc2f5997f0b932a5d8595fdfd1d0439db
jacattrongnlh <jacattrongnlh@volio.vn>
2023-04-11 09:24:03 +0700

    hide password in console log, detect operating system to run zip delete command. add a text in GUI to show progress

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import PySimpleGUI as psg
 import PySimpleGUI as sg
 
 import os
-
+import platform
 
 sg.theme("Default1")
 
@@ -13,8 +13,11 @@ layout = [
     [sg.Text("Keystore Password:"), sg.Input('', enable_events=False, key='-KEYSTOREPASSWORD-', password_char='*')],
     [sg.Text("Key Alias:"), sg.Input('', key='-KEYALIAS-')],
     [sg.Text("Alias Password:"), sg.Input('', key='-ALIASPASSWORD-', password_char='*')],
-    [sg.Button("Submit")]
+    [sg.Button("Submit")],
+    [sg.Text("", key="-RESULT-")]
 ]
+
+print(platform.system())
 
 ###Building Window
 window = sg.Window('My File Browser', layout, size=(600,250))
@@ -24,19 +27,30 @@ while True:
     if event == sg.WIN_CLOSED or event=="Exit":
         break
     elif event == "Submit":
+        window["-RESULT-"].update("Running... Please wait")
+        window.refresh()
         print(values["-AAB-"])
         print(values["-KEYSTORE-"])
         print(values["-KEYSTOREPASSWORD-"])
         print(values["-KEYALIAS-"])
         print(values["-ALIASPASSWORD-"])
-        command = "zip -d \"" + values["-AAB-"] + "\" META-INF/\\*"
+        if platform.system() == "Windows":
+            command = "7z d \"{aab_path}\" META-INF".format(aab_path=values["-AAB-"])
+        else:
+            command = "zip -d \"{aab_path}\" META-INF/\\*".format(aab_path=values["-AAB-"])
         print(command)
-        #os.system(command)
+        os.system(command)
         command = "jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore \"{key}\" \"{aab_path}\" {key_alias} -storepass {mystorepassword} -keypass {aliaspass}".format(
         	key=values["-KEYSTORE-"], 
         	aab_path=values["-AAB-"], 
         	key_alias=values["-KEYALIAS-"], 
         	mystorepassword=values["-KEYSTOREPASSWORD-"], 
         	aliaspass=values["-ALIASPASSWORD-"])
-        print(command)
+        print("jarsigner -verbose -sigalg SHA256withRSA -digestalg SHA-256 -keystore \"{key}\" \"{aab_path}\" {key_alias} -storepass {mystorepassword} -keypass {aliaspass}".format(
+            key=values["-KEYSTORE-"], 
+            aab_path=values["-AAB-"], 
+            key_alias=values["-KEYALIAS-"], 
+            mystorepassword="***", 
+            aliaspass="***"))
         os.system(command)
+        window["-RESULT-"].update("FINISHED")
